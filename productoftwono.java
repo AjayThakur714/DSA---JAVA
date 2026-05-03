b445cca8d67fdcdd1e48486014ba856e6b0bfa48
AjayThakur714 <athakur4885@gmail.com>
2026-05-03 10:24:51 +0530

    Product Of Two No

---
+++ productoftwono.java
@@ -0,0 +1,10 @@
+public class productoftwono {
+    public static void main(String[] args) {
+        int x=10;
+        int y=15;
+        int prd = x * y;
+
+        System.out.println("product of "+x+" and "+y+" is "+prd);
+
+    }
+}
