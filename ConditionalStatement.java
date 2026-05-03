1768b38f28ea9c82df7ef9805a90586191b6dac7
AjayThakur714 <athakur4885@gmail.com>
2026-05-03 11:12:37 +0530

    find even or odd

---
+++ ConditionalStatement.java
@@ -0,0 +1,14 @@
+public class ConditionalStatement {
+    public static void main(String[] args) {
+        int x =10;
+        if (x % 2 == 0)
+        {
+            System.out.println(x + " is even.");
+        }
+        else 
+        {
+            System.out.println(x + " is odd. ");
+        }
+        System.out.println("program finished ");
+        }
+}
