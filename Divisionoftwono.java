253cf977b0684881ad534ef211efde0d138c01d7
AjayThakur714 <athakur4885@gmail.com>
2026-05-03 10:44:11 +0530

    Divisions of two no

---
+++ Divisionoftwono.java
@@ -0,0 +1,14 @@
+public class Divisionoftwono {
+    public static void main(String[] args) {
+        int x= 10;
+        int y= 20;
+         
+    int v1 = x/y;
+    int v2 = y/x;
+    int v3 = x%y;
+
+    System.out.println(v1);
+    System.out.println(v2);
+    System.out.println(v3);
+    }
+}
