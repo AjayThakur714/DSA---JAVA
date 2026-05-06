0e8e32c3b3228d717716ce3f488c49f5f779e46e
AjayThakur714 <athakur4885@gmail.com>
2026-05-06 23:10:42 +0530

    Take input

---
+++ Tableprint.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Tableprint {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        for(int i=0; i<=n; i++){
+            System.out.println(i);
+    }
+    }
+}
